147461bc1312edb6f5728c69104b25007d667b82
Ayush Singh <ayushdevel1325@gmail.com>
2024-04-22 18:39:25 +0530

    Use same naming scheme for lagrange
    - Rename path to section_a/3
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- calculator_gui/urls.py
+++ calculator_gui/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("section_a/1", views.kepler_eqn, name="kepler_eqn"),
     path("section_a/2", views.kepler_hyp, name="kepler_hyp"),
+    path("section_a/3", views.lagrange, name="lagrange"),
     path("section_b/1", views.section_b_1, name="section b 1"),
     path("section_b/2", views.section_b_2, name="section b 1"),
     path("section_b/3", views.section_b_3, name="section b 1"),
@@ -15,5 +16,4 @@ urlpatterns = [
     path("section_d/2", views.lst, name="LST"),
     path("section_f", views.section_f, name="SectionF"),
     path("section_g/1", views.section_g_1, name="Hoffman"),
-    path("lagrange", views.lagrange, name="lagrange"),
 ]
