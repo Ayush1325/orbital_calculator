b6a5eb70888dd5f0d12d3534d83a2d4e21ea52e4
Ayush Singh <ayushdevel1325@gmail.com>
2024-04-23 14:29:23 +0530

    Add section e
    - Did a lot of manual changes so not sure if the code still works
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- calculator_gui/tasks/section_e.py
+++ calculator_gui/tasks/section_e.py
@@ -6,9 +6,12 @@ NAME - Ankit Kumar Singh
 ADMISSION NO - 20JE0147
 """
 
-
-import numpy as np
 import math
+import numpy as np
+
+MU = 1.327124e11
+AU = 149597871
+DEG = math.pi / 180
 
 def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
     """
@@ -16,8 +19,6 @@ def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
     vector of a planet from the date (year, month, day)
     and universal time (hour, minute, second).
     """
-    global mu
-    deg = math.pi / 180
 
     def J0(year, month, day):
         """
@@ -25,7 +26,13 @@ def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
         """
         A = math.floor(year / 100)
         B = 2 - A + math.floor(A / 4)
-        j0 = math.floor(365.25 * (year + 4716)) + math.floor(30.6001 * (month + 1)) + day + B - 1524.5
+        j0 = (
+            math.floor(365.25 * (year + 4716))
+            + math.floor(30.6001 * (month + 1))
+            + day
+            + B
+            - 1524.5
+        )
         return j0
 
     def julian_date(year, month, day, hour, minute, second):
@@ -36,16 +43,6 @@ def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
         ut = (hour + minute / 60 + second / 3600) / 24
         jd = j0 + ut
         return jd
-
-    def zero_to_360(x):
-        """
-        This function reduces the angle to within the range of 0-360 degrees.
-        """
-        if x >= 360:
-            x = x - math.floor(x / 360) * 360
-        elif x < 0:
-            x = x - (math.floor(x / 360) - 1) * 360
-        return x
 
     def kepler_E(e, M):
         """
@@ -62,22 +59,33 @@ def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
         """
         This function calculates the state vector (r,v) from the classical orbital elements (coe).
         """
-        h, e, RA, incl, w, TA, a, w_hat, L, M, E = coe
+        _h, e, _RA, _incl, _w, TA, a, _w_hat, _L, _M, E = coe
 
         # Semi-major axis (a) in km
-        a = a * au
+        a = a * AU
 
         # Eccentricity (e) correction
-        if e < 1.0e-08:
-            e = 1.0e-08
+        e = max(e, 1e-08)
 
         # Eccentric anomaly (E) in radians
-        E = E * deg
+        E = E * DEG
 
         # Calculate position and velocity vectors
-        p = a * (1 - e ** 2)
-        r = np.array([(math.cos(TA * deg) - e), (math.sin(TA * deg) * math.sqrt(1 - e ** 2)), 0]) * p / (1 + e * math.cos(TA * deg))
-        v = np.array([-math.sin(TA * deg), (e + math.cos(TA * deg)) * math.sqrt(1 - e ** 2), 0]) * math.sqrt(mu / p)
+        p = a * (1 - e**2)
+        r = (
+            np.array(
+                [
+                    (math.cos(TA * DEG) - e),
+                    (math.sin(TA * DEG) * math.sqrt(1 - e**2)),
+                    0,
+                ]
+            )
+            * p
+            / (1 + e * math.cos(TA * DEG))
+        )
+        v = np.array(
+            [-math.sin(TA * DEG), (e + math.cos(TA * DEG)) * math.sqrt(1 - e**2), 0]
+        ) * math.sqrt(MU / p)
 
         return r, v
 
@@ -95,7 +103,7 @@ def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
     a, e, incl, RA, w_hat, L = elements
 
     # Equation 2.61
-    h = math.sqrt(mu * a * (1 - e ** 2))
+    h = math.sqrt(MU * a * (1 - e**2))
 
     # Reduce the angular elements to within the range 0-360 degrees
     RA = zero_to_360(RA)
@@ -105,12 +113,14 @@ def planet_elements_and_sv(planet_id, year, month, day, hour, minute, second):
     M = zero_to_360(L - w_hat)
 
     # Algorithm 3.1 (for which M must be in radians)
-    E = kepler_E(e, M * deg)
+    E = kepler_E(e, M * DEG)
 
     # Equation 3.10 (converting the result to degrees)
-    TA = zero_to_360(2 * math.atan(math.sqrt((1 + e) / (1 - e)) * math.tan(E / 2)) / deg)
+    TA = zero_to_360(
+        2 * math.atan(math.sqrt((1 + e) / (1 - e)) * math.tan(E / 2)) / DEG
+    )
 
-    coe = [h, e, RA, incl, w, TA, a, w_hat, L, M, E / deg]
+    coe = [h, e, RA, incl, w, TA, a, w_hat, L, M, E / DEG]
 
     # Algorithm 4.2 (for which all angles must be in radians)
     r, v = sv_from_coe(coe)
@@ -123,31 +133,34 @@ def planetary_elements(planet_id):
     This function provides the J2000 orbital elements and their rates for all planets.
     """
     au = 149597871
-    deg = math.pi / 180
 
-    J2000_elements = np.array([
-        [0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084],
-        [0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973],
-        [1.00000011, 0.01671022, -0.00005, -11.26064, 102.94719, 100.46435],
-        [1.52366231, 0.09341233, 1.85061, 49.57854, 336.04084, 355.45332],
-        [5.20336301, 0.04839266, 1.30530, 100.55615, 14.75385, 34.40438],
-        [9.53707032, 0.05415060, 2.48446, 113.71504, 92.43194, 49.94432],
-        [19.19126393, 0.04716771, 0.76986, 74.22988, 170.96424, 313.23218],
-        [30.06896348, 0.00858587, 1.76917, 131.72169, 44.97135, 304.88003],
-        [39.48168677, 0.24880766, 17.14175, 110.30347, 224.06676, 238.92881]
-    ])
+    J2000_elements = np.array(
+        [
+            [0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084],
+            [0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973],
+            [1.00000011, 0.01671022, -0.00005, -11.26064, 102.94719, 100.46435],
+            [1.52366231, 0.09341233, 1.85061, 49.57854, 336.04084, 355.45332],
+            [5.20336301, 0.04839266, 1.30530, 100.55615, 14.75385, 34.40438],
+            [9.53707032, 0.05415060, 2.48446, 113.71504, 92.43194, 49.94432],
+            [19.19126393, 0.04716771, 0.76986, 74.22988, 170.96424, 313.23218],
+            [30.06896348, 0.00858587, 1.76917, 131.72169, 44.97135, 304.88003],
+            [39.48168677, 0.24880766, 17.14175, 110.30347, 224.06676, 238.92881],
+        ]
+    )
 
-    cent_rates = np.array([
-        [0.00000066, 0.00002527, -23.51, -446.30, 573.57, 538101628.29],
-        [0.00000092, -0.00004938, -2.86, -996.89, -108.80, 210664136.06],
-        [-0.00000005, -0.00003804, -46.94, -18228.25, 1198.28, 129597740.63],
-        [-0.00007221, 0.00011902, -25.47, -1020.19, 1560.78, 68905103.78],
-        [0.00060737, -0.00012880, -4.15, 1217.17, 839.93, 10925078.35],
-        [-0.00301530, -0.00036762, 6.11, -1591.05, -1948.89, 4401052.95],
-        [0.00152025, 10.00019150, -2.09, -1681.4, 1312.56, 1542547.79],
-        [-0.00125196, 0.00002514, -3.64, -151.25, -844.43, 786449.21],
-        [-0.00076912, 0.00006465, 11.07, -37.33, -132.25, 522747.90]
-    ])
+    cent_rates = np.array(
+        [
+            [0.00000066, 0.00002527, -23.51, -446.30, 573.57, 538101628.29],
+            [0.00000092, -0.00004938, -2.86, -996.89, -108.80, 210664136.06],
+            [-0.00000005, -0.00003804, -46.94, -18228.25, 1198.28, 129597740.63],
+            [-0.00007221, 0.00011902, -25.47, -1020.19, 1560.78, 68905103.78],
+            [0.00060737, -0.00012880, -4.15, 1217.17, 839.93, 10925078.35],
+            [-0.00301530, -0.00036762, 6.11, -1591.05, -1948.89, 4401052.95],
+            [0.00152025, 10.00019150, -2.09, -1681.4, 1312.56, 1542547.79],
+            [-0.00125196, 0.00002514, -3.64, -151.25, -844.43, 786449.21],
+            [-0.00076912, 0.00006465, 11.07, -37.33, -132.25, 522747.90],
+        ]
+    )
 
     J2000_coe = J2000_elements[planet_id, :]
     rates = cent_rates[planet_id, :]
@@ -177,9 +190,31 @@ def month_planet_names(month, planet_id):
     """
     This function converts the month and planet ID numbers into names for output.
     """
-    month_names = ["", "January", "February", "March", "April", "May", "June", "July",
-                   "August", "September", "October", "November", "December"]
-    planet_names = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    month_names = [
+        "",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+    planet_names = [
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune",
+    ]
 
     month_name = month_names[month]
     planet_name = planet_names[planet_id]
@@ -198,19 +233,41 @@ def main(planet_id, year, month, day, hour, minute, second):
     # second = 0
 
     # Universal gravitational parameter (mu) in km^3/s^2
-    mu = 1.327124e11
     # Convert degree to radian
-    deg = math.pi / 180
-    # AU to km
-    au = 149597871
 
     # Call the function
-    coe, r, v, jd = planet_elements_and_sv(planet_id, year, month, day, hour, minute, second)
+    coe, r, v, jd = planet_elements_and_sv(
+        planet_id, year, month, day, hour, minute, second
+    )
 
     # Convert the planet_id and month numbers into names for output
     month_name, planet_name = month_planet_names(month, planet_id)
-    
-    return(planet_name,year,month_name,day,hour,minute,second,jd,coe[0],coe[1],coe[2],coe[3],coe[4],coe[5],coe[6],coe[7],coe[8],coe[9],coe[10],r,np.linalg.norm(r),v,np.linalg.norm(v))
+
+    return (
+        planet_name,
+        year,
+        month_name,
+        day,
+        hour,
+        minute,
+        second,
+        jd,
+        coe[0],
+        coe[1],
+        coe[2],
+        coe[3],
+        coe[4],
+        coe[5],
+        coe[6],
+        coe[7],
+        coe[8],
+        coe[9],
+        coe[10],
+        r,
+        np.linalg.norm(r),
+        v,
+        np.linalg.norm(v),
+    )
 
     # Echo the input data and output the solution to the command window
     # print("---------------------------------------------------")
